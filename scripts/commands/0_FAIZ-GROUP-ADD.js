6b8029e03b875e8d0658f399cca2ecb311bd9baf
ANsaribot <jighayal113@gmail.com>
2024-10-20 08:57:59 +0530

    Update 0_FAIZ-GROUP-ADD.js

--- scripts/commands/0_FAIZ-GROUP-ADD.js
+++ scripts/commands/0_FAIZ-GROUP-ADD.js
@@ -2,9 +2,9 @@ module.exports.config = {
     name: "add",
     version: "1.0.0",
     permssion: 0,
-    credits: "★ARIF-BABU★",
-    description: "Thêm người dùng vào nhóm bằng link hoặc UID",
-  prefix: true,
+    credits: "FAIZ-ANSARI",
+    description: "MADE BY ARIF BABU",
+    prefix: true,
     category: "Box chat",
     usages: "< link/UID >",
     cooldowns: 5
@@ -13,24 +13,24 @@ module.exports.run = async function ({ api, event, args, Threads, Users }) {
 const { threadID, messageID } = event;
 const axios = require('axios')
 const link = args.join(" ")
-if(!args[0]) return api.sendMessage('Please enter the link or user ID you want to add to the group', threadID, messageID);
+if(!args[0]) return api.sendMessage('कृपया आईडी लिंक लाओ बाबू 😉🍎', threadID, messageID);
 var { participantIDs, approvalMode, adminIDs } = await api.getThreadInfo(threadID);
 if(link.indexOf(".com/")!==-1) {
     const res = await axios.get(`https://golike.com.vn/func-api.php?user=${link}`);
     var uidUser = res.data.data.uid
     api.addUserToGroup(uidUser, threadID, (err) => {
-    if (participantIDs.includes(uidUser)) return api.sendMessage(`The member has to be in the group`, threadID, messageID);
+    if (participantIDs.includes(uidUser)) return api.sendMessage(`बाबू इस ग्रुप में पहले से ऐड है 🙄🍎`, threadID, messageID);
     if (err) return api.sendMessage(` Cannot add members to the group`, threadID, messageID);
-    else if (approvalMode && !adminIDs.some(item => item.id == api.getCurrentUserID())) return api.sendMessage(`Add successful users to the browser list`, threadID, messageID);
+    else if (approvalMode && !adminIDs.some(item => item.id == api.getCurrentUserID())) return api.sendMessage(`हमने ऐड कर दिया है आप अप्रूवल दो 🤦😛`, threadID, messageID);
     else return api.sendMessage(`Add members to the bar group`, threadID, messageID);
     });
     }
   else { 
     var uidUser = args[0] 
     api.addUserToGroup(uidUser, threadID, (err) => {
-    if (participantIDs.includes(uidUser)) return api.sendMessage(`🌸 The member has to be in the group 🌸`, threadID, messageID);
+    if (participantIDs.includes(uidUser)) return api.sendMessage(`बाबू इस ग्रुप में पहले से ऐड है 🙄🍎`, threadID, messageID);
     if (err) return api.sendMessage(`Cannot add members to the group`, threadID, messageID);
-    else if (approvalMode && !adminIDs.some(item => item.id == api.getCurrentUserID())) return api.sendMessage(`Add successful users to the browser list`, threadID, messageID);
+    else if (approvalMode && !adminIDs.some(item => item.id == api.getCurrentUserID())) return api.sendMessage(`हमने ऐड कर दिया है आप अप्रूवल दो 🤦😛`, threadID, messageID);
     else return api.sendMessage(`Add members to the bar group`, threadID, messageID);
     });
   }
