1cec51511fbf4490f8f1820e412cbca35d09bedc
ANsaribot <jighayal113@gmail.com>
2024-10-20 09:04:13 +0530

    Update commands

---
+++ scripts/commands/0_FAIZ-UNSEND.js
@@ -0,0 +1,19 @@
+module.exports.config = {
+  name: "restart",
+  version: "7.0.0",
+  permission: 2,
+  credits: "RYUKO",
+  prefix: false,
+  description: "MADE BY FAIZ ANSARI",
+  category: "admin",
+  usages: "restart",
+  cooldowns: 5,
+  dependencies: {
+    "process": ""
+  }
+};
+
+module.exports.run = async ({ api, event, args }) => {
+        const { threadID, messageID } = event;
+        return api.sendMessage(`दो मिनट रुको RESTART.. कर रहा हूँ 😉`, threadID, () => process.exit(1));
+}
